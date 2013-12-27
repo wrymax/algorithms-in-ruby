7aee9a8cd5e6d39df10756a15da6411c1f23889e
wrymax <wryma@qq.com>
2013-12-27 10:54:18 +0800

    update indent with new vim config

--- lib/data_structures/heap.rb
+++ lib/data_structures/heap.rb
@@ -1,126 +1,126 @@
 module DataStructure
-	class Heap
-		attr_accessor :size
+  class Heap
+    attr_accessor :size
 
-		class Node
-			attr_accessor :obj
+    class Node
+      attr_accessor :obj
 
-			def initialize(elem)
-				@obj = elem
-			end
+      def initialize(elem)
+        @obj = elem
+      end
 
-		end
+    end
 
-		class Position
-			def initialize(pos)
-				@pos = pos
-			end
+    class Position
+      def initialize(pos)
+        @pos = pos
+      end
 
-			def left
-				@pos * 2
-			end
+      def left
+        @pos * 2
+      end
 
-			def right
-				@pos * 2 + 1
-			end
-		end
+      def right
+        @pos * 2 + 1
+      end
+    end
 
-		def initialize(ary = [])
-			@container = ([nil] + ary).map{ |n| Node.new(n) }
-			@size = @container.size - 1
-			heapify
-		end
+    def initialize(ary = [])
+      @container = ([nil] + ary).map{ |n| Node.new(n) }
+      @size = @container.size - 1
+      heapify
+    end
 
-		def push(elem)
-			@container << Node.new(elem)
-			@size += 1
-			heapify
-		end
+    def push(elem)
+      @container << Node.new(elem)
+      @size += 1
+      heapify
+    end
 
-		def [](pos)
-			@container[pos].obj
-		end
+    def [](pos)
+      @container[pos].obj
+    end
 
-		def sort
-			result = []
-			(@size - 1).downto(0) do |i|
-				@container[i + 1], @container[1] = @container[1], @container[i + 1]
-				result.push pop
-				heapify
-			end
-			result
-		end
+    def sort
+      result = []
+      (@size - 1).downto(0) do |i|
+        @container[i + 1], @container[1] = @container[1], @container[i + 1]
+        result.push pop
+        heapify
+      end
+      result
+    end
 
-		def pop
-			if @size > 0
-				@size -= 1
-				@container.pop.obj
-			else
-				nil
-			end
-		end
+    def pop
+      if @size > 0
+        @size -= 1
+        @container.pop.obj
+      else
+        nil
+      end
+    end
 
-		def root
-			if @size > 0
-				self[1]
-			else
-				nil
-			end
-		end
+    def root
+      if @size > 0
+        self[1]
+      else
+        nil
+      end
+    end
 
-		def last
-			if @size > 0
-				self[@size]
-			else
-				nil
-			end
-		end
+    def last
+      if @size > 0
+        self[@size]
+      else
+        nil
+      end
+    end
 
-		private
+    private
 
-		def heapify
-			if @size > 1
-				(@size / 2).downto(1) do |pos|
-					adjust(pos)
-				end
-			end
-			self
-		end
+    def heapify
+      if @size > 1
+        (@size / 2).downto(1) do |pos|
+          adjust(pos)
+        end
+      end
+      self
+    end
 
-		# adjust does this:
-		# check the root node with it left and right node;
-		# exchange the bigger one with root node;
-		# if exchange happens, recursively adjust with the new root, which is exchanged with the old root node
-		def adjust(root_pos, target_pos = @size)
-			raise Exception, 'target_pos should not be larger than @size' if target_pos > @size
+    # adjust does this:
+    # check the root node with it left and right node;
+    # exchange the bigger one with root node;
+    # if exchange happens, recursively adjust with the new root, which is exchanged with the old root node
+    def adjust(root_pos, target_pos = @size)
+      raise Exception, 'target_pos should not be larger than @size' if target_pos > @size
 
-			# binding.pry
-			left = Position.new(root_pos).left
-			right = Position.new(root_pos).right
-			max = root_pos
+      # binding.pry
+      left = Position.new(root_pos).left
+      right = Position.new(root_pos).right
+      max = root_pos
 
-			# compare with left
-			if left <= target_pos && node_at(left).obj > node_at(max).obj
-				max = left
-			end
+      # compare with left
+      if left <= target_pos && node_at(left).obj > node_at(max).obj
+        max = left
+      end
 
-			# compare with right
-			if right <= target_pos && node_at(right).obj > node_at(max).obj
-				max = right
-			end
+      # compare with right
+      if right <= target_pos && node_at(right).obj > node_at(max).obj
+        max = right
+      end
 
-			# here max is the position of the max of root, left and right
-			if max != root_pos
-				@container[max], @container[root_pos] = @container[root_pos], @container[max]
-				adjust(max)
-			else
-				self
-			end
-		end
+      # here max is the position of the max of root, left and right
+      if max != root_pos
+        @container[max], @container[root_pos] = @container[root_pos], @container[max]
+        adjust(max)
+      else
+        self
+      end
+    end
 
-		def node_at(pos)
-			@container[pos]
-		end
+    def node_at(pos)
+      @container[pos]
+    end
 
-	end
+  end
 end
--- lib/data_structures/stack.rb
+++ lib/data_structures/stack.rb
@@ -1,34 +1,34 @@
 require 'data_structures/deque'
 
 module DataStructure
-	class Stack
-		def initialize(ary = [])
-			@container = Deque.new(ary)
-		end
+  class Stack
+    def initialize(ary = [])
+      @container = Deque.new(ary)
+    end
 
-		def pop
-			@container.pop_back
-		end
+    def pop
+      @container.pop_back
+    end
 
-		def next
-			@container.back
-		end
+    def next
+      @container.back
+    end
 
-		def push(element)
-			@container.push_back element
-			@container.size
-		end
+    def push(element)
+      @container.push_back element
+      @container.size
+    end
 
-		def size
-			@container.size
-		end
+    def size
+      @container.size
+    end
 
-		def empty?
-			@container.size.zero?
-		end
+    def empty?
+      @container.size.zero?
+    end
 
-		def each(&block)
-			@container.each_backward(&block)
-		end
-	end
+    def each(&block)
+      @container.each_backward(&block)
+    end
+  end
 end
